3527bc462cdacf4ad0407cbc38bb684e5c1235d3
savin ilya <savin15ilya3112@gmail.com>
2026-03-08 23:01:53 +0700

    added card component, card display, and card styles

--- js/main.js
+++ js/main.js
@@ -4,11 +4,42 @@ Vue.component('column-component', {
         <div class="column">
             <h2> {{ columnTitle }} </h2>
             
+            <card-component
+            v-for="card in columnCards"
+            :key="card.id"
+            :card-data="card">
+            </card-component>
         </div>
     `,
     computed: {
         columnCards() {
             return this.allCards.filter(card => card.column === this.columnId)
+        }
+    }
+})
+
+Vue.component('card-component', {
+    props: ['cardData'],
+    template: `
+        <div class="card">
+            <h3> {{ cardData.title }} </h3>
+            <p class="card-description"> {{ cardData.description }} </p>
+            <div class="card-meta">
+                <p>Создано: {{ formatDate(cardData.createdAt) }} </p>
+                <p>Дедлайн: {{ formatDate(cardData.deadline) }} </p>
+            </div>
+        </div>
+    `,
+    methods: {
+        formatDate(dateString) {
+            const date = new Date(dateString);
+            return date.toLocaleString('ru-RU', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit'
+            })
         }
     }
 })
